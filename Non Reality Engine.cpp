a75b0099816d6d65baa44181d2aa75a9a250ffae
Cassie Parsons <Joseph.Parsons2003@outlook.com>
2024-10-08 15:51:43 +0100

    Added Initial files

---
+++ Non Reality Engine.cpp
@@ -0,0 +1,12 @@
+﻿// Non Reality Engine.cpp : Defines the entry point for the application.
+//
+
+#include "Non Reality Engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
