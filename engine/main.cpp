8e3f99f887d4c60b1743a3b6a1c76bee57ebe121
Cassie Parsons <Joseph.Parsons2003@outlook.com>
2024-10-18 12:39:36 +0100

    Added vertex shader and shader base class

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 
 #include <engine/Window.h>
+#include <engine/Shader.h>
 
 int main()
 {
 	std::unique_ptr<Window> window = std::make_unique<Window>("Test window", 800, 600);
+
+	Microsoft::WRL::ComPtr<ID3D11Device> device = window->getDevice();
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> deviceContext = window->getDeviceContext();
+
+
+
+	VertexShader vs{ device.Get(),L"shaders/vertex.hlsl" };
 
 	while (!window->getWindowShouldClose())
 	{
