9a48231c131fecf25c8a9673f7d0f7fdd3c08d74
Cassie Parsons <Joseph.Parsons2003@outlook.com>
2024-10-18 16:05:41 +0100

    Meshes are now renderable

--- engine/main.cpp
+++ engine/main.cpp
@@ -2,6 +2,8 @@
 
 #include <engine/Window.h>
 #include <engine/Shader.h>
+#include <engine/InputLayout.h>
+#include <engine/Mesh.h>
 
 int main()
 {
@@ -10,13 +12,88 @@ int main()
 	Microsoft::WRL::ComPtr<ID3D11Device> device = window->getDevice();
 	Microsoft::WRL::ComPtr<ID3D11DeviceContext> deviceContext = window->getDeviceContext();
 
+	Mesh mesh{};
 
+	Mesh::MeshVertex vertices[3]
+	{
+		{	//Vertex 0
+			DirectX::XMFLOAT3(0.0f,0.5f,0.1f),
+			DirectX::XMFLOAT3(0,0, 1),
+			DirectX::XMFLOAT2(0.5,0.0)
+		},
+		{	//Vertex 1
+			DirectX::XMFLOAT3(0.5f,-0.5f,0.1f),
+			DirectX::XMFLOAT3(0,0, 1),
+			DirectX::XMFLOAT2(1.0,1.0)
+		},
+		{	//Vertex 2
+			DirectX::XMFLOAT3(-0.5f,-0.5f,0.1f),
+			DirectX::XMFLOAT3(0,0, 1),
+			DirectX::XMFLOAT2(0.0,1.0)
+		},
+	};
+
+	int indices[3]
+	{
+		0,1,2
+	};
+
+
+	mesh.addVertices(device.Get(), vertices, 3, indices, 3, false);
+
+	InputLayout inputLayout{};
 
 	VertexShader vs{ device.Get(),L"shaders/vertex.hlsl" };
+	PixelShader ps{ device.Get(),L"shaders/fragment.hlsl" };
+
+	D3D11_INPUT_ELEMENT_DESC inputArr[3]
+	{
+		{
+			"POSITION",
+			0,
+			DXGI_FORMAT_R32G32B32_FLOAT,
+			0,
+			0,
+			D3D11_INPUT_PER_VERTEX_DATA,
+			0
+		},
+		{
+			"NORMAL",
+			0,
+			DXGI_FORMAT_R32G32B32_FLOAT,
+			0,
+			sizeof(float) * 3,
+			D3D11_INPUT_PER_VERTEX_DATA,
+			0
+		},
+		{
+			"TEXCOORD",
+			0,
+			DXGI_FORMAT_R32G32_FLOAT,
+			0,
+			sizeof(float) * 6,
+			D3D11_INPUT_PER_VERTEX_DATA,
+			0
+		},
+	};
+
+	inputLayout.addInputLayout(device.Get(), vs.getByteCode(), inputArr, 3);
 
 	while (!window->getWindowShouldClose())
 	{
 		window->clearBackBuffer();
+		window->bindRenderTarget();
+
+		inputLayout.useInputLayout(deviceContext.Get());
+		mesh.useMesh(deviceContext.Get());
+
+		vs.bindShader(deviceContext.Get());
+		ps.bindShader(deviceContext.Get());
+
+		deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+		
+
+		deviceContext->DrawIndexed(3, 0, 0);
 
 		window->presentBackBuffer();
 	}
