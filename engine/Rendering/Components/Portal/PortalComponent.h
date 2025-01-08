eac5a2eb0ba4ea91951205ab4100dd8140eec766
Cassie Parsons <Joseph.Parsons2003@outlook.com>
2025-01-08 20:08:55 +0000

    Remvoed PortalCameraComponent
    Portal rendering is now set up using a single component

--- engine/Rendering/Components/Portal/PortalComponent.h
+++ engine/Rendering/Components/Portal/PortalComponent.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <engine/ObjectStructure/RenderComponent.h>
+#include <engine/Rendering/ObliqueView.h>
+
+class Scene;
 
 class PortalComponent : public RenderComponent
 {
@@ -10,10 +13,21 @@ public:
 	PortalComponent(PortalComponent&& other);
 	~PortalComponent();
 
-	void linkPortal(PortalComponent* other);
 	PortalComponent* getOtherPortal() { return linkedPortal;}
+
+	void linkPortal(PortalComponent* other);
+	void setPlayer(GameObject* player);
+	void SetScene(Scene* scene) { this->scene = scene; }
 	
 	void Render(bool RenderPortals) override;
 protected:
+	DirectX::XMFLOAT4X4 calcMatrix();
+
 	PortalComponent* linkedPortal;
+
+	ObliqueView obliqueView;
+
+
+	GameObject* player;
+	Scene* scene;
 };
