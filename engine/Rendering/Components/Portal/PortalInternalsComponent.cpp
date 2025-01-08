8b0c77801a43224d340913648f3595aa758e5b24
Cassie Parsons <Joseph.Parsons2003@outlook.com>
2025-01-08 19:40:21 +0000

    RemovedPortalDrawComponent

--- engine/Rendering/Components/Portal/PortalInternalsComponent.cpp
+++ engine/Rendering/Components/Portal/PortalInternalsComponent.cpp
@@ -1,6 +1,7 @@
 #include "PortalInternalsComponent.h"
 
 #include <engine/ObjectStructure/GameObject.h>
+#include <engine/Rendering/Components/MeshComponent.h>
 
 #include "PortalComponent.h"
 
@@ -27,7 +28,16 @@ void PortalInternalsComponent::Render(bool RenderPortals)
 	else
 	{
 		scene->renderLayer(otherPortalScene, false);
-
 	}
+
+	Mesh* mesh = gameObject->getComponent<MeshComponent>()->getMesh();
+
+	DirectX::XMMATRIX worldMatrix = gameObject->getComponent<TransformComponent>()->calcWorldMatrix();
+	DirectX::XMFLOAT4X4 worldMatrixSta;
+	DirectX::XMStoreFloat4x4(&worldMatrixSta, worldMatrix);
+
+	renderer->addRenderCall<DrawPortal>({ 0,mesh,worldMatrixSta });
+
+	renderer->addRenderCall<PortalEnd>({ 0 });
 
 }
